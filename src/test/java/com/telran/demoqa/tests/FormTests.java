f45f5fb9c584b4d631b1eb83e542e221c474d601
akutafina <anastasia.kutafina@trivago.com>
2022-12-06 21:58:58 +0100

    Lesson #3

---
+++ src/test/java/com/telran/demoqa/tests/FormTests.java
@@ -0,0 +1,35 @@
+package com.telran.demoqa.tests;
+
+import com.telran.demoqa.data.Student;
+import com.telran.demoqa.pages.FormPage;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class FormTests extends BaseTests{
+    @BeforeMethod
+    public void ensurePreconditions(){
+        new FormPage(driver).navigateWithURL();
+    }
+
+    @Test
+    public void testStudentRegistrationFormPositive(){
+        // todo: add Data Provider from file
+        Student validStudent = new Student("Test First Name",
+                "Test Last Name",
+                "test@email.com",
+                Student.Gender.MALE,
+                "1234567890" ,
+                "11 May 1990",
+                "src/test/testdata/photo.jpeg");
+
+        new FormPage(driver)
+                .enterFirstName(validStudent.getFirstName())
+                .enterLastName(validStudent.getLastName())
+                .enterEmail(validStudent.getEmail())
+                .selectGender(validStudent.getGender())
+                .enterPhoneNumber(validStudent.getPhone())
+                .enterDateOfBirth(validStudent.getDateOfBirth())
+                .uploadPicture(validStudent.getPictureLink());
+
+    }
+}
