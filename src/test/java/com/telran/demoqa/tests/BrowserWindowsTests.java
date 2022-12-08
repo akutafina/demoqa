f488f6901abbc92a69af6203fc7d2b394801c77f
akutafina <anastasia.kutafina@trivago.com>
2022-12-08 21:48:06 +0100

    Lesson #4.1

---
+++ src/test/java/com/telran/demoqa/tests/BrowserWindowsTests.java
@@ -0,0 +1,37 @@
+package com.telran.demoqa.tests;
+
+import com.telran.demoqa.pages.BrowserWindowsPage;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class BrowserWindowsTests extends BaseTests{
+
+    @BeforeMethod
+    public void ensurePreconditions() {
+        new BrowserWindowsPage(driver).goToWithURL();
+    }
+
+    @Test
+    public void openNewTabTestPositive(){
+        String actualSampleHeadingText = new BrowserWindowsPage(driver)
+                .clickOnNewTabBtn()
+                    .getSampleHeadingText();
+
+        Assert.assertEquals(actualSampleHeadingText, "This is a sample page",
+                "Actual heading text is: " + actualSampleHeadingText);
+    }
+
+
+    @Test
+    public void openNewWindowTestPositive(){
+        String actualSampleHeadingText = new BrowserWindowsPage(driver)
+                .clickOnNewWindowBtn()
+                .getSampleHeadingText();
+
+        Assert.assertEquals(actualSampleHeadingText, "This is a sample page",
+                "Actual heading text is: " + actualSampleHeadingText);
+    }
+
+    //HW: add test for 'New Window Message' btn
+}
