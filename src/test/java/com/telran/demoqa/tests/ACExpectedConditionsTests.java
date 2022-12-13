07fa1c0b4b9fa8a30b544baea291b9e3ade3a881
akutafina <anastasia.kutafina@trivago.com>
2022-12-13 22:02:18 +0100

    13.12.22

---
+++ src/test/java/com/telran/demoqa/tests/ACExpectedConditionsTests.java
@@ -0,0 +1,39 @@
+package com.telran.demoqa.tests;
+
+import com.telran.demoqa.pages.ACExpectedConditionsPage;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class ACExpectedConditionsTests extends BaseTests{
+
+
+    @BeforeMethod
+    public void ensurePreconditions(){
+        new ACExpectedConditionsPage(driver).goWithURL();
+    }
+
+    @Test
+    public void showAlertTest(){
+        String actualAlertHandledBadgeText = new ACExpectedConditionsPage(driver)
+                .interactWithShowAlert()
+                .getAlertHandledBageText();
+        Assert.assertEquals(actualAlertHandledBadgeText, "Alert handled",
+                "The Alert handling text appeared: " + actualAlertHandledBadgeText);
+    }
+
+    @Test
+    public void acceptPromtTest(){
+        String actualPromptHandledBadgeText =
+                new ACExpectedConditionsPage(driver)
+                .acceptPromptAlert()
+                .getPromtHandledBageText();
+        Assert.assertEquals(actualPromptHandledBadgeText, "OK",
+                "The Prompt handling text appeared: " + actualPromptHandledBadgeText);
+    }
+
+    @Test
+    public void cancelPromtTest(){
+        //todo: HW
+    }
+}
