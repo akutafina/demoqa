bf336f314b67de445ed8a3b9efa830b9b5c932c6
akutafina <anastasia.kutafina@trivago.com>
2022-12-20 22:08:46 +0100

    Builder

--- src/test/java/com/telran/demoqa/tests/ACLoginTests.java
+++ src/test/java/com/telran/demoqa/tests/ACLoginTests.java
@@ -2,6 +2,7 @@ package com.telran.demoqa.tests;
 
 import com.telran.demoqa.pages.ACLoginPage;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 public class ACLoginTests extends BaseTests{
@@ -18,9 +19,22 @@ public class ACLoginTests extends BaseTests{
                 .loginWithKeyboard("test_login", "test_pwd");
     }
 
+    @Parameters({"login", "password"})
+    @Test
+    public void loginWithParamsNegative(String login, String pwd){
+        new ACLoginPage(driver)
+                .loginWithKeyboard(login, pwd);
+    }
+
     @Test
     public void loginWithKeyboardCopyPasteTestNegative(){
         new ACLoginPage(driver)
                 .loginWithKeyboardCopyPaste("test_login");
     }
+
+    @Test
+    public void loginPositive(){
+        new ACLoginPage(driver);
+    }
+
 }
